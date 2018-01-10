2ad5a07d571562bc5ad6a71beda658563aec109e
Louis Houghton <louis@louishoughton.com>
2018-01-10 10:55:08 +0000

    Add clearer logging around idam calls

--- app/user/user-resolver.js
+++ app/user/user-resolver.js
@@ -1,6 +1,9 @@
 const config = require('config')
 const fetch = require('../util/fetch')
 
+const {logging} = require('../logging/dm-logger')
+const logger = logging.getLogger('user-resolver.js')
+
 const getTokenDetails = (jwt) => {
   const bearerJwt = jwt.startsWith('Bearer ') ? jwt : 'Bearer ' + jwt
 
@@ -8,8 +11,16 @@ const getTokenDetails = (jwt) => {
     headers: {
       'Authorization': bearerJwt
     }
-  })
-    .then(res => res.json())
+  }).then(res => res.json())
+    .catch(error => {
+      logger.warn(JSON.stringify({
+        errorMessage: 'Non 200 status received from IDAM when authenticating user. Is your token valid?',
+        statusText: error.statusText,
+        status: error.status,
+        url: error.url
+      }))
+      throw error
+    })
 }
 
 exports.getTokenDetails = getTokenDetails
