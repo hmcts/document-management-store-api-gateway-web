1d4d841c3ade7d936a927685b4cc3fc7bce0234d
Louis Houghton <louis@louishoughton.com>
2018-01-10 11:24:17 +0000

    Linting!!!

--- spec/user/auth-checker-user-only-filter.spec.js
+++ spec/user/auth-checker-user-only-filter.spec.js
@@ -5,42 +5,42 @@ const expect = chai.expect
 const sinonChai = require('sinon-chai')
 chai.use(sinonChai)
 
-describe('User Resolver', () => {
+describe('Auth Checker User Only Filter', () => {
   let filter,
     userRequestAuthorizer
 
   beforeEach(() => {
-      userRequestAuthorizer = {
-          authorise: sinon.stub()
-      }
+    userRequestAuthorizer = {
+      authorise: sinon.stub()
+    }
 
-      filter = proxyquire('../../app/user/auth-checker-user-only-filter', {
+    filter = proxyquire('../../app/user/auth-checker-user-only-filter', {
       './user-request-authorizer': userRequestAuthorizer
     })
   })
 
   it('should add user to req', (done) => {
-      const req = {originalUrl: '/documents'}
-      const res = {}
-      const next = () => {
-        expect(req.authentication.user).to.equal('12345')
-          done()
-      }
+    const req = {originalUrl: '/documents'}
+    const res = {}
+    const next = () => {
+      expect(req.authentication.user).to.equal('12345')
+      done()
+    }
 
-      userRequestAuthorizer.authorise.returns(Promise.resolve('12345'))
+    userRequestAuthorizer.authorise.returns(Promise.resolve('12345'))
 
     filter(req, res, next)
   })
 
   it('should add 401 status if authentication fails', (done) => {
-      const req = {originalUrl: '/documents'}
-      const res = {}
-      const next = (error) => {
-        expect(error.status).to.equal(401)
-          done()
-      }
+    const req = {originalUrl: '/documents'}
+    const res = {}
+    const next = (error) => {
+      expect(error.status).to.equal(401)
+      done()
+    }
 
-      userRequestAuthorizer.authorise.returns(Promise.reject(new Error()))
+    userRequestAuthorizer.authorise.returns(Promise.reject(new Error()))
 
     filter(req, res, next)
   })
