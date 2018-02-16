b617949adb11e2d8e602ed6df3cd4c457ec9326e
Alec Doran-Twyford <alec@alectronic.co>
2018-02-16 10:52:00 +0000

    cnp updates

--- infrastructure/variables.tf
+++ infrastructure/variables.tf
@@ -1,18 +1,29 @@
 variable "product" {
     type    = "string"
     default = "dm"
+    description           = "The name of your application"
 }
 
+
 variable "location" {
-    type    = "string"
-    default = "UK South"
+    type                  = "string"
+    default               = "UK South"
 }
 
 variable "env" {
-    type = "string"
+    type                  = "string"
+    description           = "(Required) The environment in which to deploy the application infrastructure."
 }
 
-variable "infrastructure_env" {
-    default     = "dev"
-    description = "Infrastructure environment to point to"
+variable "ilbIp" {}
+
+/* variable "tenant_id" {
+  type                  = "string"
+  description           = "(Required) The Azure Active Directory tenant ID that should be used for authenticating requests to the key vault. This is usually sourced from environemnt variables and not normally required to be specified."
 }
+variable "object_id" {
+  type                  = "string"
+  description           = "(Required) The object ID of a user, service principal or security group in the Azure Active Directory tenant for the vault. The object ID must be unique for the list of access policies. This is usually sourced from environemnt variables and not normally required to be specified."
+} */
+
+variable "subscription" {}
